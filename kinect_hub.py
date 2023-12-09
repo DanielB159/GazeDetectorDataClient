15dfdf9dfd5e1cba0d66923412ed672a198b6395
daniel <daniel.b159357@gmail.com>
2023-12-09 17:51:39 +0200

    implemented initial camera kinect functionality when kinect camera is connected

--- kinect_hub.py
+++ kinect_hub.py
@@ -1,5 +1,10 @@
 """Module with kinect hub class and methods"""
-from pykinect_azure.k4a import _k4a
+#pylint: disable=no-member
+import pykinect_azure as pykinect
+# import numpy as np
+import cv2
+from pykinect_azure.k4a import Device, Capture, Image
+from PIL import ImageTk, Image as PILImage
 from imports import tk
 
 
@@ -19,6 +24,7 @@ class KinectHub:
             self._is_initialized = True
             self.root = root
             self.top_level = tk.Toplevel(root)
+            self.top_level.resizable(False, False)
             self.top_level.title("Kinect Hub")
             self.top_level.geometry("500x500")
             self.top_level.protocol("WM_DELETE_WINDOW", self.__del__)
@@ -26,10 +32,47 @@ class KinectHub:
 
     def define_ui(self):
         """Function defining the UI of the Kinect Hub"""
-        label = tk.Label(self.top_level, text="Kinect Hub")
-        label.pack()
+        title = tk.Label(self.top_level, text="Kinect Hub", fg="red", font=("Helvetica", 16))
+        title.grid(row=0, column=10, columnspan=2, sticky="ew")
+        live_view_btn = tk.Button(self.top_level, text="Live View", command=self.live_view)
+        live_view_btn.grid(row=2, column=2, columnspan=1, sticky="ew")
+
 
     def __del__(self):
         self.top_level.destroy()
         self._instance = None
         self._is_initialized = False
+
+    def live_view(self):
+        """Function to open the live view in the kinect camera"""
+        device: Device = self.configure_camera()
+        cv2.namedWindow("Live View", cv2.WINDOW_NORMAL)
+        while True:
+            # Get a capture from the device
+            capture: Capture = device.update()
+            ret: bool
+            raw_color_image: Image
+            # Get the color image from the capture
+            ret, raw_color_image = capture.get_color_image()
+
+            # if the capture did not succeed, then continue
+            if not ret:
+                continue
+            
+            image = cv2.putText(raw_color_image, "Live View", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+            cv2.imshow("Live View", image)
+
+            # Press q to exit
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+    def configure_camera(self) -> Device:
+        """Function to configure the camera"""
+        pykinect.initialize_libraries()
+        # Modify camera configuration
+        device_config = pykinect.default_configuration
+        device_config.color_resolution = pykinect.K4A_COLOR_RESOLUTION_1080P
+        # device_config.depth_mode = pykinect.K4A_DEPTH_MODE_WFOV_2X2BINNED
+
+        return pykinect.start_device(config=device_config)
+
