c9a325a5a09b0fc63f74c299755b76b9f8c997d6
gal199922 <gal199922@gmail.com>
2023-12-12 17:56:42 +0200

    work on glasseshub

--- glasses_hub.py
+++ glasses_hub.py
@@ -1,6 +1,9 @@
 """Module with glasses hub class and methods"""
 from PyQt5.QtWidgets import QPushButton, QWidget, QLabel
 from qasync import QEventLoop
+from imports import asyncio
+from imports import g3pylib
+from imports import os
 
 class GlassesHub:
     """Class representing the Kinect Hub"""
@@ -28,6 +31,29 @@ class GlassesHub:
         self._instance = None
         self._is_initialized = False
         del self
+
+    async def connect(self):
+        """Function to connect machine to glasses"""
+        self.g3 = await g3pylib.connect_to_glasses.with_hostname(os.environ["G3_HOSTNAME"])
+        self.connection_label.setText(f"Connected to {self.g3.rtsp_url}")
+    
+    async def disconnect(self):
+        self.g3.close()
+        self.connection_label.setText("Not Connected")
     
     def define_ui(self):
         """Function defining the UI of the Glasses Hub"""
+        self.connection_label : QLabel = QLabel(self.glasses_widget)
+        self.connection_label.setText("Not Connected")
+        self.connection_label.move(200, 0)
+
+        self.connect_button : QPushButton = QPushButton(self.glasses_widget)
+        self.connect_button.setText("Connect")
+        self.connect_button.move(200, 100)
+        self.connect_button.clicked.connect(lambda: asyncio.ensure_future(self.connect()))
+
+        
+        self.connect_button : QPushButton = QPushButton(self.glasses_widget)
+        self.connect_button.setText("Disconnect")
+        self.connect_button.move(200, 200)
+        self.connect_button.clicked.connect(lambda: asyncio.ensure_future(self.disconnect()))
--- imports.py
+++ imports.py
@@ -1,4 +1,4 @@
-import tkinter as tk
 import g3pylib
 import os
 import dotenv
+import asyncio
